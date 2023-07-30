6c822865490f7e01c2dfb34da1874b307bfb34bf
SathiyamoorthiMurugesan <sathya08ece@gmail.com>
2023-07-31 00:27:26 +0530

    Abstract class and OOPs basic

---
+++ src/test/java/abstractLearning/DemoAbstract1.java
@@ -0,0 +1,41 @@
+package abstractLearning;
+
+public abstract class DemoAbstract1 implements TestInterface {
+	
+	int c = 20;
+	static int d;
+	
+	static String e = "abc";
+	
+	protected int cd = 40;
+	private int de = 50;
+	
+
+	public static void main(String[] args) {
+
+	}
+	
+	public void method1() {
+		int a = 10;
+	}
+
+	public static void method2() {
+		int b;
+	}
+
+	public int method3() {
+
+		return 0;
+	}
+
+	public static String method4(char c) {
+
+		return null;
+	}
+	
+//	public void method5();
+	public abstract void method5();
+	
+//	public static abstract void method6();
+
+}
